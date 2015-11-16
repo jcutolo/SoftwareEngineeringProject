04be9daa033bcf589fa3d9fc253884d27cf9ab95
jcutolo <jcutolo@broncos.uncfsu.edu>
2015-11-16 15:42:33 -0500

    Continued work on the Implementation Code

--- Implementation Workflow/main.cpp
+++ Implementation Workflow/main.cpp
@@ -13,150 +13,182 @@
 
 #include <cstdlib>
 #include <vector>
-#include <iostream>
-
+#include <string>
 using namespace std;
-
 class Member {
 public:
     struct newMember {
-        string memberName[25];
+        string memberName;
         int memberID;
-        string memberStreetAddress[25];
-        string memberCity[14];
-        string memberState[2];
+        string memberStreetAddress;
+        string memberCity;
+        string memberState;
         int memberZipCode;
     };
     vector<newMember> memberContainer;
-    Member(string name, int ID, string streetAddress, string city, string state, int zipCode) {
-        newMember.memberName = name;
-        newMember.memberID = ID;
-        newMember.memberStreetAddress = streetAddress;
-        newMember.memberCity = city;
-        newMember.memberState = state;
-        newMember.memberZipCode = zipCode;
-        memberContainer.push_back(newMember);
+    newMember newestMember;
+    Member(string &name, int &ID, string &streetAddress, string &city, string &state, int &zipCode) {
+        newestMember.memberName = name;
+        newestMember.memberID = ID;
+        newestMember.memberStreetAddress = streetAddress;
+        newestMember.memberCity = city;
+        newestMember.memberState = state;
+        newestMember.memberZipCode = zipCode;
+        memberContainer.push_back(newestMember);
     }
+    string verifyMember(int ID);
 };
 
-class Provider {
-public:
-    struct newProvider {
-        string providerName[25];
-        int providerID;
-        string providerStreetAddress[25];
-        string providerCity[14];
-        string providerState[2];
-        int providerZipCode;
-    };
-    vector<newProvider> providerContainer;
-    Provider(string name, int ID, string streetAddress, string city, string state, int zipCode) {
-        newProvider.providerName = name;
-        newProvider.providerID = ID;
-        newProvider.providerStreetAddress = streetAddress;
-        newProvider.providerCity = city;
-        newProvider.providerState = state;
-        newProvider.providerZipCode = zipCode;
-        providerContainer.push_back(newProvider);
-    }
-};
-
-class Operator {
-public:
-    void addMember(string name, int ID, string streetAddress, string city, string state, int zipCode) {
-        Member newMember = new Member(name, ID, streetAddress, city, state, zipCode);
-    }
-    void deleteMember(int ID) {
-        //cycle through the member container to delete the specified member by their ID
-        //if no ID matches the ID given an error message is returned
-    }
-    void updateMember() {
-        int memberID;
-        cout << "\nPlease enter the ID of the member you are would like to update: ";
-        cin >> memberID;
-        //now cycle through the member container to find the specified member
-        //once found the operator is asked which information he/she would like to update
-        //if no ID matches the one given to the function an error message is returned
-    }
-    void addProvider(string name, int ID, string streetAddress, string city, string state, int zipCode) {
-        Provider newProvider = new Provider(name, ID, streetAddress, city, state, zipCode);
-    }
-    void deleteProvider(int ID) {
-        //cycle through the provider container to delete the specified provider by their ID
-        //if no ID matches the ID given an error message is returned    
-    }
-    void updateProvider(int ID) {
-        int providerID;
-        cout << "\nPlease enter the ID of the provider you would like to update: ";
-        cin >> providerID;
-        //now cycle through the provider container to find the specified provider
-        //once found the operator is asked which information he/she would like to update
-        //if no ID matches the one given to the function an error message is returned
-    }
-};
-
-class Fee {
-public:
-    int serviceCode;
-    double fee;
-    string name[30];
-    //need to add a providerDirectory of the services with codes, fees, and their names
-};
-
-class Claim {
-public:
-    string currentDate[10];
-    string currentTime[8];
-    string dateServiceProvided[10];
-    // need to add a function to allow the provider to enter in the correct information for a claim
-};
-
-class Report {
-public:
-    string type;
-    void printMemberReport(string type);
-    void printProviderReport(string type);
-    void printManagerReport(string type);
-    // need to add a function that will allow the data center to print the specified reports each week or upon request
-};
-//need to place this function in the correct spot either the provider class or the providerController class
-string verifyProvider(int providerID) {
-    //verify the providerID that was entered is an actual provider for ChocAn
-    //if it is found allow the program to continue
-    //if it is not found the program will return an error message
-}
-
-//need to place this function in the correct spot either the member class or the memberController class
-string verifyMember(int memberID) {
+string Member::verifyMember(int ID) {
     //verify the memberID that was entered is an actual member for ChocAn
     // if it found the program displays the member's status
     //if it is not found the program displays an error message
 }
 
-void userInterface() {
+class Provider {
+public:
+    struct newProvider {
+        string providerName;
+        int providerID;
+        string providerStreetAddress;
+        string providerCity;
+        string providerState;
+        int providerZipCode;
+    };
+    vector<newProvider> providerContainer;
+    newProvider newestProvider;
+    Provider(string &name, int &ID, string &streetAddress, string &city, string &state, int &zipCode) {
+        newestProvider.providerName = name;
+        newestProvider.providerID = ID;
+        newestProvider.providerStreetAddress = streetAddress;
+        newestProvider.providerCity = city;
+        newestProvider.providerState = state;
+        newestProvider.providerZipCode = zipCode;
+        providerContainer.push_back(newestProvider);
+    }
+    string verifyProvider(int ID);
+};
+
+string Provider::verifyProvider(int ID) {
+    //verify the providerID that was entered is an actual provider for ChocAn
+    //if it is found allow the program to continue
+    //if it is not found the program will return an error message   
+}
+
+class Operator {
+public:
+    void addMember(string name, int ID, string streetAddress, string city, string state, int zipCode);
+    void deleteMember(int ID);
+    void updateMember(int ID);
+    void addProvider(string name, int ID, string streetAddress, string city, string state, int zipCode);
+    void deleteProvider(int ID);
+    void updateProvider(int ID);
+};
+
+void Operator::addMember(string name, int ID, string streetAddress, string city, string state, int zipCode) {
+    Member* newMember = new Member(name, ID, streetAddress, city, state, zipCode);
+}
+
+void Operator::deleteMember(int ID) {
+    //find member by ID provided and delete them from the vector
+}
+
+void Operator::updateMember(int ID) {
+    int memberID;
+    std::cout << "\nPlease enter the ID of the member you are would like to update: ";
+    std::cin >> memberID;
+    //now cycle through the member container to find the specified member
+    //once found the operator is asked which information he/she would like to update
+    //if no ID matches the one given to the function an error message is returned*/
+}
+
+void Operator::addProvider(string name, int ID, string streetAddress, string city, string state, int zipCode) {
+    Provider* newProvider = new Provider(name, ID, streetAddress, city, state, zipCode);
+}
+
+void Operator::deleteProvider(int ID) {
+    //find provider by ID provided and delete them from the vector
+}
+
+void Operator::updateProvider(int ID) {
+    int providerID;
+    std::cout << "\nPlease enter the ID of the provider you would like to update: ";
+    std::cin >> providerID;
+    //now cycle through the provider container to find the specified provider
+    //once found the operator is asked which information he/she would like to update
+    //if no ID matches the one given to the function an error message is returned*/
+}
+
+class Fee {
+public:
+    struct newFee {
+        int serviceCode;
+        double fee;
+        string name;
+    };
+    newFee newestFee;
+    vector<newFee> providerDirectory;
+    Fee(int code, double Fee, string feeName) {
+        newestFee.serviceCode = code;
+        newestFee.fee = Fee;
+        newestFee.name = feeName;
+        providerDirectory.push_back(newestFee);
+    }
+};
+
+class Claim {
+public:
+    struct newClaim {
+        string currentDate;
+        string currentTime;
+        string dateServiceProvided;
+    };
+    newClaim newestClaim;
+    Claim(string &cDate, string &cTime, string &dateProvided) {
+        newestClaim.currentDate = cDate;
+        newestClaim.currentTime = cTime;
+        newestClaim.dateServiceProvided = dateProvided;
+    }
+};
+
+class Report {
+public:
+    void printMemberReport();
+    void printProviderReport();
+    void printManagerReport();
+};
+
+class UserInterface {
+public:
     int providerNumber; 
     int memberID;
-    string currentDate[10];
-    string currentTime[8];
-    string dateServiceProvided[10];
+    string currentDate;
+    string currentTime;
+    string dateServiceProvided;
     int serviceCode;
-    string comments[100];
+    string comments;
     
-    cout << "Please enter your provider number: ";
-    cin >> providerNumber;
-    //verifyProvider(providerNumber);
-    cout << "\nNow enter the member's identification number: ";
-    cin >> memberID;
-    //verifyMember(memberID);
-    cout << "\nIn order to be billed for your services you must first re-enter the member's identification number: ";
-    cin >> memberID;
-    //verifyMember(memberID);
-    cout << "\nEnter the six-digit service code corresponding to the service that was provided: ";
-    cin >> serviceCode;
+    void userInterface(void);
+};
+
+void UserInterface::userInterface(void) {
+    Provider newProvider;
+    Member newMember;
+    std::cout << "\nPlease enter your provider number: ";
+    std::cin >> providerNumber;
+    newProvider.verifyProvider(providerNumber);
+    std::cout << "\nNow enter the member's identification number: ";
+    std::cin >> memberID;
+    newMember.verifyMember(memberID);
+    std::cout << "\nIn order to be billed for your services you must first re-enter the member's identification number: ";
+    std::cin >> memberID;
+    newMember.verifyMember(memberID);
+    std::cout << "\nEnter the six-digit service code corresponding to the service that was provided: ";
+    std::cin >> serviceCode;
     //ProviderDirectory(serviceCode);
-    cout << "\nEnter any additional comments about the service provided to the member: ";
-    cin >> comments;
-    //Send the comments and any other information to a specific function to hold
+    std::cout << "\nEnter any additional comments about the service provided to the member: ";
+    std::cin >> comments;
+    //Send the comments and any other information to a specific function to hold*/
 }
 /*
  * 
@@ -172,7 +204,8 @@ int main(void) {
     newOperator.addProvider("Diana Sawyer", 000000003, "6873 Thomas Court", "Aberdeen", "NC", 28315);
     newOperator.addProvider("Tammy Smith", 000000004, "5893 Main Street", "Hope Mills", "NC", 28348);
     
-    userInterface();
+    UserInterface newUserInterface;
+    newUserInterface.userInterface();
     return 0;
 }
 
